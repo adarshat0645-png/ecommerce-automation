e10cf985c55bcb2a5eff3c063b2eb08599036203
adarshat0645-png <adarshat0645@gmail.com>
2026-07-08 22:50:35 +0530

    Create login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    # Locators
+    USERNAME_FIELD = (By.ID, "user-name")
+    PASSWORD_FIELD = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR_MESSAGE = (By.CSS_SELECTOR, "[data-test='error']")
+
+    # Actions
+    def enter_username(self, username):
+        self.driver.find_element(*self.USERNAME_FIELD).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(*self.PASSWORD_FIELD).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(*self.LOGIN_BUTTON).click()
+
+    def login(self, username, password):
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login()
+
+    def get_error_message(self):
+        return self.driver.find_element(*self.ERROR_MESSAGE).text
